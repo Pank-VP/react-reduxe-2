cfaa84da8508cf4b5e7816cb0f488c40d2d18750
Deadbill <vp_pank@mail.ru>
2022-03-03 22:05:20 +0300

    markdown

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,15 +3,19 @@ import { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { getPlaces } from '../../store/Places/ActionCreators';
-import pluralize from 'pluralize';
 import formatDate from '../../utils/formatDate';
 import DateFormat from '../../constants/DateFormat';
 import { PlaceType } from '../../api/Places';
-import { Cabin, DirectionsTransit, Security } from '@mui/icons-material/';
-import { Tooltip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import PlaceTypeIcon from '../../components/PlaceTypeIcon/PlaceTypeIcon';
+import formatCapacity from '../../utils/formatCapacity';
+import PageLayout from '../PlaceDetailse/PageLayout/PageLayout';
 
-
+const mapType = (type: PlaceType) => {
+  return (
+  <PlaceTypeIcon type={type} />
+ );
+};
 
 const Home: FC = () => {
   const dispatch = useDispatch();
@@ -21,22 +25,9 @@ const Home: FC = () => {
   useEffect(() => {
     dispatch(getPlaces());
   }, []);
-
+  
   const openDetails = (id: string) => {
     navigate(`/places/${id}`)
-  };
-
-  const mapType = (type: PlaceType) => {
-
-    return (
-      <Tooltip title={type}>
-        <>
-          {type === PlaceType.Basement && <Cabin />}
-          {type === PlaceType.Bunker && <Security />}
-          {type === PlaceType.Metro && <DirectionsTransit />}
-        </>
-      </Tooltip>
-    );
   };
 
   const rows = items?.map((place) => {
@@ -44,8 +35,8 @@ const Home: FC = () => {
       id: place.id,
       address: place.address,
       type: place.type,
-      capacity: `${place.capacity} ${pluralize('person', place.capacity)}`,
-      createdAt: formatDate(place.createdAt, DateFormat.ShortDate),
+      capacity: formatCapacity(place.capacity),
+      createdAt: formatDate(place.createdAt, DateFormat.ShortDate)
     };
   });
 
@@ -57,16 +48,18 @@ const Home: FC = () => {
   ];
 
   return (
-    <div style={{ height: 500 }}>
-      <DataGrid
-        loading={!rows}
-        rows={rows || []}
-        columns={COLUMNS}
-        pageSize={7}
-        isRowSelectable={() => false}
-        onRowClick={(params) => openDetails(params.id.toString())}
-      />
-    </div>
+    <PageLayout>
+      <div style={{ height: 500 }}>
+        <DataGrid
+          loading={!rows}
+          rows={rows || []}
+          columns={COLUMNS}
+          pageSize={7}
+          isRowSelectable={() => false}
+          onRowClick={(params) => openDetails(params.id.toString())}
+        />
+      </div>
+    </PageLayout>
   );
 };
 
